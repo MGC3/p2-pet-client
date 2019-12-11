1f97f6d4d76aa7a97b6c0cec89b877af031b711f
michaelchun <michael.chun03@gmail.com>
2019-12-11 15:55:47 -0500

    Stub update name fields and button - receives correct ID when clicked, but not new name yet

--- assets/scripts/pet-show/events.js
+++ assets/scripts/pet-show/events.js
@@ -7,8 +7,15 @@ const onDeletePet = e => {
   console.warn("You are deleting pet with id: ", id);
 };
 
+const onUpdatePetName = e => {
+  e.preventDefault();
+  const id = $(e.target).data("id");
+  console.warn("You are updating pet with id: ", id);
+};
+
 const addHandlers = () => {
   $("#app").on("click", ".pet-delete__btn", onDeletePet);
+  $("#app").on("submit", "#update-pet-name", onUpdatePetName);
 };
 
 module.exports = {
