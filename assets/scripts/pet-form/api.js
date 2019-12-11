dbd8d7d479e09216d74e739c43157cd771a355a4
michaelchun <michael.chun03@gmail.com>
2019-12-11 13:39:46 -0500

    Fix backend reject issue by sending auth token with request

--- assets/scripts/pet-form/api.js
+++ assets/scripts/pet-form/api.js
@@ -1,9 +1,13 @@
 const config = require("../config");
+const store = require("../store");
 
 const add = formData => {
   return $.ajax({
     url: config.apiUrl + "/pets/",
     method: "POST",
+    headers: {
+      Authorization: `Token token=${store.user.token}`
+    },
     data: formData
   });
 };
