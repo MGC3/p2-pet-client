05d52c024fa20871961d7c337c519c8bf466bd8e
michaelchun <michael.chun03@gmail.com>
2019-12-13 15:21:06 -0500

    Stub chart shows up in Pet page

--- assets/scripts/user/ui.js
+++ assets/scripts/user/ui.js
@@ -1,18 +1,19 @@
 const petForm = require("../templates/PetForm.handlebars");
 const petShow = require("../templates/PetShow.handlebars");
 const commonUi = require("../common/ui");
+const petChart = require("../chart/petWeightChart");
 
 const loadPetForm = () => {
   $("#app").html(petForm);
 };
 
 const getPetSuccess = data => {
-  console.warn("before sort: ", data);
   // attrib sort code from: https://flaviocopes.com/how-to-sort-array-of-objects-by-property-javascript/
   // sort by newest date at top
   data.pet.weightlogs.sort((a, b) => (a.date < b.date ? 1 : -1));
   const petShowHtml = petShow({ pet: data.pet });
   $("#app").html(petShowHtml);
+  petChart.drawChart();
 };
 
 const getPetFailure = () => {
