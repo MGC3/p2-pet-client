f94e14f7bad13f4779446ff89c2bfa25848230d5
michaelchun <michael.chun03@gmail.com>
2019-12-14 15:01:20 -0500

    Add logo link from auth navbar

--- assets/scripts/user-form/events.js
+++ assets/scripts/user-form/events.js
@@ -1,5 +1,6 @@
 const ui = require("./ui");
 const commonUi = require("../common/ui");
+const commonEvents = require("../common/events");
 
 const onSignUpLink = () => {
   ui.onSignUpLinkClick();
@@ -13,10 +14,15 @@ const onLogoLanding = () => {
   commonUi.loadLanding();
 };
 
+const onLogoAuth = () => {
+  commonEvents.onGetUserHome();
+};
+
 const addHandlers = () => {
   $("body").on("click", "#sign-up-link", onSignUpLink);
   $("body").on("click", "#sign-in-link", onSignInLink);
   $("body").on("click", "#logo-landing", onLogoLanding);
+  $("body").on("click", "#logo-auth", onLogoAuth);
 };
 
 module.exports = {
