773baceaeda54f7b3dd241a066143a74f819e0b1
michaelchun <michael.chun03@gmail.com>
2019-12-14 12:01:22 -0500

    Update webpack sass loader to properly find node_modules, for material design
    I was trying to import an MD component and ran into an error where the sass loader couldnt find the npm folder. I tried working around this by adding a ~ in front, but then ran into the issue of being unable to find any of the other dependencies required by the first file. This was resolved by editing the webpack config file to look at node modules (it looked like it was already doing this...but maybe not?).

--- grunt/webpack.js
+++ grunt/webpack.js
@@ -1,30 +1,30 @@
-'use strict'
+"use strict";
 
-const webpack = require('webpack')
-const path = require('path')
+const webpack = require("webpack");
+const path = require("path");
 
 module.exports = {
   options: {
     entry: {
-      application: './index.js',
-      vendor: ['jquery']
+      application: "./index.js",
+      vendor: ["jquery"]
     },
 
     output: {
-      filename: '[name].js',
-      path: path.join(__dirname, '/../public'),
-      publicPath: 'public/'
+      filename: "[name].js",
+      path: path.join(__dirname, "/../public"),
+      publicPath: "public/"
     },
 
     plugins: [
       new webpack.optimize.CommonsChunkPlugin({
-        name: 'vendor',
+        name: "vendor",
         minChunks: Infinity
       }),
       new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery'
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery"
       })
     ],
 
@@ -33,59 +33,58 @@ module.exports = {
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          loader: 'babel-loader',
+          loader: "babel-loader",
           query: {
-            presets: ['env']
+            presets: ["env"]
           }
         },
         {
           test: /\.css$/,
-          use: [
-            { loader: 'style-loader' },
-            { loader: 'css-loader?url=false' }
-          ]
+          use: [{ loader: "style-loader" }, { loader: "css-loader?url=false" }]
         },
         {
           test: /\.scss$/,
           use: [
-            { loader: 'style-loader' },
-            { loader: 'css-loader',
+            { loader: "style-loader" },
+            {
+              loader: "css-loader",
               options: {
                 url: false
               }
             },
             {
-              loader: 'postcss-loader', // Run post css actions
+              loader: "postcss-loader", // Run post css actions
               options: {
-                plugins: function () { // post css plugins, can be exported to postcss.config.js
-                  return [
-                    require('autoprefixer')
-                  ]
+                plugins: function() {
+                  // post css plugins, can be exported to postcss.config.js
+                  return [require("autoprefixer")];
                 }
               }
-            }, {
-              loader: 'sass-loader',
+            },
+            {
+              loader: "sass-loader",
               options: {
-                includePaths: [
-                  path.resolve(__dirname, './node_modules')
-                ]
+                includePaths: ["./node_modules"]
+                // had to edit this to get material design loading properly, not sure why
+                //  includePaths: [path.resolve(__dirname, "./node_modules")]
               }
-            }]
+            }
+          ]
         },
         {
           test: /\.woff[\d]?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+          loader: "url-loader?limit=10000&mimetype=application/font-woff"
         },
         {
           test: /\.(ttf|eot|svg|png|jpg|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-          loader: 'file-loader'
+          loader: "file-loader"
         },
         {
           test: /\.(hbs|handlebars)$/,
-          loader: 'handlebars-loader',
+          loader: "handlebars-loader",
           query: {
             helperDirs: [
-              path.join(__dirname, '/../assets/scripts/templates/helpers')
+              path.join(__dirname, "/../assets/scripts/templates/helpers")
             ]
           }
         }
@@ -94,7 +93,7 @@ module.exports = {
 
     resolve: {
       alias: {
-        handlebars: 'handlebars/dist/handlebars.js'
+        handlebars: "handlebars/dist/handlebars.js"
       }
     },
     stats: {
@@ -109,4 +108,4 @@ module.exports = {
     watch: false,
     keepalive: false
   }
-}
+};
