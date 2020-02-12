6ea8ad5b25a3c26c08e0b1bdd22a927242138927
michaelchun <michael.chun03@gmail.com>
2020-02-12 01:54:50 -0500

    Change deploy script for new favicon

--- grunt/shell.js
+++ grunt/shell.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 // This command will check if `.gitignore` exists, and ensure that git isn't
 // tracking `node_modules`. If both of the above are true, the deploy will
@@ -9,19 +9,15 @@ const checkGitIgnore = `
    then true;
    else printf "\n\nWARNING: .gitignore is wrong or missing.
    Ask an instructor for assistance!\n\n" && false; fi
-`
+`;
 
-const ghPagesList = [
-  'index.html',
-  'favicon.ico',
-  'public'
-].join(' ')
+const ghPagesList = ["index.html", "favicon-32x32.png", "public"].join(" ");
 
 module.exports = {
-  'check-gitignore': {
+  "check-gitignore": {
     command: checkGitIgnore
   },
-  'git-is-clean': {
+  "git-is-clean": {
     // `$(git status --porcelain)` will evaluate to the empty string if the
     // working directory is clean.
     // `test -z` will exit 0 (true) if its argument is an empty string.
@@ -29,29 +25,29 @@ module.exports = {
     // repository isn't clean and exit false causing the grunt tasks to end.
     command: 'test -z "$(git status --porcelain)"  || (git status && false)'
   },
-  'git-push-master': {
+  "git-push-master": {
     // if the push to master fails, we want to delete any files that were created
     // by the build process and exit all remaining build steps
-    command: 'git push origin master || (git clean -f && false)'
+    command: "git push origin master || (git clean -f && false)"
   },
-  'git-checkout-master': {
-    command: 'git checkout master'
+  "git-checkout-master": {
+    command: "git checkout master"
   },
-  'deploy-prepare': {
+  "deploy-prepare": {
     command: [
       'git branch -D gh-pages || echo "so not removed"',
-      'git checkout --orphan gh-pages',
-      'git rm --cached \'*\''
-    ].join(' && ')
+      "git checkout --orphan gh-pages",
+      "git rm --cached '*'"
+    ].join(" && ")
   },
-  'deploy-publish': {
+  "deploy-publish": {
     command: [
-      'touch .nojekyll',
+      "touch .nojekyll",
       `git add --force .nojekyll ${ghPagesList}`,
       'git commit -m "deploy task"',
-      'git push origin gh-pages --force',
-      'git clean -x -d --force --exclude=node_modules',
-      'git checkout master'
-    ].join(' && ')
+      "git push origin gh-pages --force",
+      "git clean -x -d --force --exclude=node_modules",
+      "git checkout master"
+    ].join(" && ")
   }
-}
+};
